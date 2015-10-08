e26cfa34b53ed1ce02887ef5221b92b24d8afbf0
Poren Chiang <ren.chiang@gmail.com>
2015-10-08 22:05:36 +0800

    Set up config and queue objects

--- xinpu.py
+++ xinpu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from datetime import datetime, timedelta
+from queue import Queue
 import json
 import threading
 import trace
@@ -9,15 +10,26 @@ import sys
 Event to inform threads to stop.
 '''
 app = threading.Event()
-config = {}
+config = None
 
-class Site(object):
-    def __init__(self, name=None, url=None, interval=None, options=None):
-        self.name = name
-        self.url = url
-        self.interval = interval or 0
-        self.options = options or {}
-        self.last_updated = config['last_updated'] or (datetime.now() - timedelta(seconds=config['backtrack']))
+class Config(object):
+    def __init__(self, **kwargs):
+        self.username = kwargs.get('username')
+        self.format = kwargs.get('format', '({site}) {url} ({title}): {summary}')
+        self.throttle = kwargs.get('throttle', 5)
+        self.backtrack = kwargs.get('backtrack', 0)
+        self.last_updated = kwargs.get('last_updated') or (datetime.now() - timedelta(seconds=self.backtrack))
+        self.feeds = []
+        for feed in kwargs.get('feeds') or []:
+            self.feeds.append(Feed(**feed))
+
+class Feed(object):
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.url = kwargs.get('url')
+        self.interval = kwargs.get('interval', 0)
+        self.options = kwargs.get('options')
+        self.last_updated = config.last_updated
 
     def needs_update(self):
         return (datetime.now() - self.last_updated).total_seconds() >= self.interval
@@ -28,12 +40,28 @@ class FeedCrawler(threading.Thread):
         self.daemon = True
 
     def run(self):
-        while app.wait(timeout=)
+        while not app.wait(timeout=config.throttle):
+            for feed in config.feeds:
+                if not feed.needs_update():
+                    continue
+                # Fetch new feed
+
+    def post_item(self, item):
+        raise NotImplementedError('No event handler provided')
 
 class ContentPoster(threading.Thread):
     def __init__(self):
         super(ContentPoster, self).__init__(name='poster')
         self.daemon = True
+        self.queue = Queue()
+
+    def run(self):
+        while not app.wait(timeout=config.throttle):
+            if self.queue.empty():
+                continue
+
+            item = self.queue.get()
+            # Post item
 
 def launch():
     trace.info('starting up.')
@@ -41,9 +69,14 @@ def launch():
         # Read settings file
         configure()
 
-        # Launch threads
+        # Configure threads
         crawler = FeedCrawler()
         poster = ContentPoster()
+        crawler.post_item = lambda item: poster.queue.put(item)
+
+        # Launch threads
+        crawler.start()
+        poster.start()
 
         # Block until any key pressed
         sys.stdin.read()
@@ -53,24 +86,11 @@ def launch():
 
 def configure():
     with open('config.json', 'r') as f:
-        # Set some important defaults
-        entity = {
-            'format': '({site}) {url} ({title}): {summary}',
-            'throttle': 5,
-            'backtrack': 0,
-        }
-
         # Read configuration from file
-        entity.update(json.load(f))
-
-        # Load sites
-        sites = []
-        for site in entity['feeds']:
-            sites.append(Site(**site))
-        entity['sites'] = sites
+        entity = json.load(f)
 
         # Save to global configuration
-        global config = entity
+        global config = Config(**entity)
 
 if __name__ == '__main__':
     threading.current_thread().name = 'main'
