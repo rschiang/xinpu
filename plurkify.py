98908498fbb2cf0da6fe249ee084749c2fa18faf
Poren Chiang <ren.chiang@gmail.com>
2015-10-11 21:24:20 +0800

    Fix script removal script

--- plurkify.py
+++ plurkify.py
@@ -1,4 +1,5 @@
 import bleach
+import html
 import re
 
 ALLOWED_TAGS = (
@@ -6,9 +7,13 @@ ALLOWED_TAGS = (
 )
 
 ALLOWED_ATTRIBUTES = {
-    'a': ('href',),
-    'img': ('src', 'alt'),
+    'a': ['href'],
+    'img': ['src', 'alt'],
 }
+
+SCRIPTINATOR = re.compile(r'\<script[^\>]*\>.*?\</script\>', re.IGNORECASE | re.DOTALL)
+
+WHITESPACEINATOR = re.compile(r'\s{2,}')
 
 LINKIFIER = re.compile(r'\<a href=[\'"]?(?P<url>[^\'"\>]+)[\'"]?\>(?P<title>[^\>]+?)\</a\>', re.IGNORECASE)
 
@@ -25,6 +30,14 @@ FORMATTERS = {
     '\n': ('br', 'p'),
 }
 
+def remove_scripts(text):
+    return SCRIPTINATOR.sub('', text)
+
+def remove_spaces(text):
+    def formatter(match):
+        return '\n' if '\n' in match.group(0) else ' '
+    return WHITESPACEINATOR.sub(formatter, text)
+
 def linkify(text):
     return LINKIFIER.sub(r'\g<url> (\g<title>)', text)
 
@@ -40,9 +53,10 @@ def reformat(text):
         return ''
     return TAG_GRINDER.sub(formatter, text).strip()
 
+def clean_tags(text):
+    return bleach.clean(text, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True)
+
 def plurkify_text(text):
-    raw_text = bleach.clean(text, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True)
-    raw_text = linkify(raw_text)
-    raw_text = linkify_image(raw_text)
-    raw_text = reformat(raw_text)
-    return html.unescape(raw_text)
+    for process in (remove_scripts, clean_tags, linkify, linkify_image, reformat, html.unescape, remove_spaces):
+        text = process(text)
+    return text
