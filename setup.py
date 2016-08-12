26e0de86345846aa1c8e47880bceebe706f6a926
Poren Chiang <ren.chiang@gmail.com>
2016-08-13 00:35:35 +0800

    Adding meta

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup
 
-setup(name='xinpu',
-      version='0.1.0',
-      description='Publish news from RSS feeds to Plurk',
-      url='https://github.com/rschiang/xinpu',
-      author='Poren Chiang',
-      author_email='ren.chiang@gmail.com',
-      license='BSD',
-      packages=['xinpu'],
-      install_requires=[
-            'feedparser',
-            'python-dateutil',
-            'beautifulsoup4',
-      ],
-      dependency_links=[
-            'git+https://github.com/clsung/plurk-oauth.git#egg=plurk_oauth',
-      ],
-      zip_safe=False)
+def readme():
+      with open('README.md', 'r') as f:
+            return f.read()
+
+meta = {
+      'name': 'xinpu',
+      'version': '0.1.0',
+      'description': 'Publish news from RSS feeds to Plurk',
+      'long_description': readme(),
+      'url': 'https://github.com/rschiang/xinpu',
+      'author': 'Poren Chiang',
+      'author_email': 'ren.chiang@gmail.com',
+      'license': 'BSD',
+      'packages': ['xinpu'],
+      'install_requires': ['feedparser', 'python-dateutil', 'beautifulsoup4'],
+      'dependency_links': ['git+https://github.com/clsung/plurk-oauth.git#egg=plurk_oauth'],
+      'include_package_date': True,
+      'zip_safe': False,
+}
+
+setup(**meta)
